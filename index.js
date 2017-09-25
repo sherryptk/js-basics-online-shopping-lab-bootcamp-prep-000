ab1256695a84ae2e89f7fae97869a9f67d803bf8
Sherry <sherryptk@gmail.com>
2017-09-25 14:35:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function removeFromCart(item) {
       cart.splice(i, 1)
       return cart
     } else {
-      console.log('That item is not in your cart')
+      console.log('That item is not in your cart.')
     }
 
   }
