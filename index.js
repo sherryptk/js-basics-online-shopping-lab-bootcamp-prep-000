6601d9598496702516c1b086f9df57669bd14593
Sherry <sherryptk@gmail.com>
2017-09-25 15:49:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,4 +65,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber===undefined) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+    
+  }
 }
