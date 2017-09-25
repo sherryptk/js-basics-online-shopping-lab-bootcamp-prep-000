d61dcd544f1aa334c868fa8968addd047eaf1d64
Sherry <sherryptk@gmail.com>
2017-09-25 14:22:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function total() {
 
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
-    if (Object.keys(cart[i]).hasOwnProperty(item)) {
+    if ((cart[i]).hasOwnProperty(item)) {
       cart.splice(i, 1)
       return cart
       // delete cart.item
