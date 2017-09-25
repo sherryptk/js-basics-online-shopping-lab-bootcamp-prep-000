6db7d4cf250e326a60411e18440272ba0ec741b1
Sherry <sherryptk@gmail.com>
2017-09-25 15:54:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
   else {
-    var total=0
-    total=total()
+    //var total=0
+    var total=total()
     console.log("Your total cost is $" + total + ", which will be charged to the card "+ cardNumber".")
     cart=[]
   }
